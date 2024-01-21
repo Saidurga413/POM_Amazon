e61986ca6983ee46ba08b9e8e8a1236dcbeb8f78
amit45963 <amit.k@gormalone.com>
2024-01-21 11:39:41 +0530

    'newcommit'

--- Amazon/src/test/java/Amazon/ProductSearchAndAddInCart/Test/ProductSearchAndAddInCartTest.java
+++ Amazon/src/test/java/Amazon/ProductSearchAndAddInCart/Test/ProductSearchAndAddInCartTest.java
@@ -33,18 +33,22 @@ public class ProductSearchAndAddInCartTest extends WebDriverUtils{
 		
 		//span[@class='a-size-medium a-color-base a-text-normal']
 		List<WebElement> gggg = driver.findElements(By.xpath("//span[@class='a-size-medium a-color-base a-text-normal']"));
-//		for(WebElement ggh : gggg) {
-//			System.out.println(ggh.getText());
-//		}
-		
-		for (WebElement webElement1 : gggg) {
-			System.out.println(webElement1.getText());
-			if(webElement1.getText().trim().equals("HP Pavilion 15, Ryzen 5 5625U, 15.6 inch(39.6cm) FHD Laptop, 8GB RAM,512GB SSD, AMD Radeon Graphics,B&O Audio, Backlit Keypad(Win 11,Alexa,1.75 Kgs,Natural Silver) 15-eh2050au")) {
-				webElement1.click();
-				break;
-			}
+		for(WebElement ggh : gggg) {
+			System.out.println(ggh.getText());
 		}
 		
+//		for (WebElement webElement1 : gggg) {
+//			System.out.println(webElement1.getText());
+//			if(webElement1.getText().trim().equals("HP Pavilion 15, Ryzen 5 5625U, 15.6 inch(39.6cm) FHD Laptop, 8GB RAM,512GB SSD, AMD Radeon Graphics,B&O Audio, Backlit Keypad(Win 11,Alexa,1.75 Kgs,Natural Silver) 15-eh2050au")) {
+//				webElement1.click();
+//				break;
+//			}
+//		}
+		//div[contains(@class,'rush-component s-featured-result-item')]//span[@class='a-price-whole'][normalize-space()='44,990']
+		Thread.sleep(2000);
+		WebElement list = driver.findElement(By.xpath("//div[contains(@class,'rush-component s-featured-result-item')]//span[@class='a-price-whole'][normalize-space()='44,990']"));
+		list.click();
+		Thread.sleep(5000);
 		
 		Set<String> windowHandles11 = driver.getWindowHandles();
 		System.out.println(windowHandles11);
